83424adfbd903e272866b66ba5eb8fd6a356187e
radaaai <nastya.mills2021@gmail.com>
2023-11-25 04:08:10 +0200

    ще трохи рефакторінгу та коментарі

--- src/GameField.java
+++ src/GameField.java
@@ -8,33 +8,63 @@ import java.util.Random;
 
 public class GameField extends JPanel implements ActionListener {
 
+    // Константи для розмірів поля, розміру точки та кількості точок
     private static final int SIZE = 320, DOT_SIZE = 16, ALL_DOTS = 400;
+
+    // Початкова кількість точок гравця, затребувані для ініціалізації гри
     private static final int INITIAL_DOTS = 3;
+
+    // Затребуване значення затримки таймера (в мілісекундах)
     private static final int TIMER_DELAY = 250;
 
+    // Зображення для точок та яблук
     private Image dot, apple;
+
+    // Координати яблука
     private int appleX, appleY;
+
+    // Масиви координат точок гравця
     private int[] x = new int[ALL_DOTS], y = new int[ALL_DOTS];
+
+    // Кількість точок гравця та таймер для оновлення гри
     private int dots;
     private Timer timer;
+
+    // Напрямки руху гравця та статус гри
     private boolean left, right = true, up, down, inGame = true;
 
+    // Конструктор класу
     public GameField() {
+        // Встановлення чорного кольору фону
         setBackground(Color.black);
+
+        // Завантаження зображень
         loadImages();
+
+        // Ініціалізація гри
         initGame();
+
+        // Додавання слухача клавіш
         setKeyAdapter();
+
+        // Налаштування фокусу на поле гри
         setFocusable(true);
+
+        // Встановлення розміру поля гри
         setPreferredSize(new Dimension(SIZE, SIZE));
+
+        // Ініціалізація таймера для автоматичного оновлення гри
         initializeTimer();
     }
 
+    // Ініціалізація гри
     private void initGame() {
         dots = INITIAL_DOTS;
         initializeSnakeCoordinates();
         createApple();
     }
 
+    // Ініціалізація координат точок гравця
     private void initializeSnakeCoordinates() {
         for (int i = 0; i < dots; i++) {
             x[i] = 48 - i * DOT_SIZE;
@@ -42,11 +72,13 @@ public class GameField extends JPanel implements ActionListener {
         }
     }
 
+    // Створення нового яблука
     private void createApple() {
         appleX = new Random().nextInt(20) * DOT_SIZE;
         appleY = new Random().nextInt(20) * DOT_SIZE;
     }
 
+    // Завантаження зображень
     private void loadImages() {
         ImageIcon appleIcon = new ImageIcon("apple.png");
         apple = appleIcon.getImage();
@@ -54,11 +86,13 @@ public class GameField extends JPanel implements ActionListener {
         dot = dotIcon.getImage();
     }
 
+    // Ініціалізація таймера
     private void initializeTimer() {
         timer = new Timer(TIMER_DELAY, this);
         timer.start();
     }
 
+    // Малювання компоненту
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -69,6 +103,7 @@ public class GameField extends JPanel implements ActionListener {
         }
     }
 
+    // Малювання елементів гри
     private void drawGame(Graphics g) {
         g.drawImage(apple, appleX, appleY, this);
         for (int i = 0; i < dots; i++) {
@@ -76,12 +111,15 @@ public class GameField extends JPanel implements ActionListener {
         }
     }
 
+    // Малювання напису "Game Over"
     private void drawGameOver(Graphics g) {
         String str = "Game Over";
         g.setColor(Color.white);
+        // Центрування напису по горизонталі
         g.drawString(str, SIZE / 2 - 50, SIZE / 2);
     }
 
+    // Рух гравця
     private void move() {
         for (int i = dots; i > 0; i--) {
             x[i] = x[i - 1];
@@ -90,6 +128,7 @@ public class GameField extends JPanel implements ActionListener {
         updateSnakeCoordinates();
     }
 
+    // Оновлення координат точок гравця в залежності від напрямку руху
     private void updateSnakeCoordinates() {
         if (left) {
             x[0] -= DOT_SIZE;
@@ -105,6 +144,7 @@ public class GameField extends JPanel implements ActionListener {
         }
     }
 
+    // Перевірка з'їденого яблука
     private void checkApple() {
         if (x[0] == appleX && y[0] == appleY) {
             dots++;
@@ -112,6 +152,7 @@ public class GameField extends JPanel implements ActionListener {
         }
     }
 
+    // Перевірка зіткнень
     private void checkCollisions() {
         for (int i = dots; i > 0; i--) {
             if (i > 4 && x[0] == x[i] && y[0] == y[i]) {
@@ -124,6 +165,7 @@ public class GameField extends JPanel implements ActionListener {
         }
     }
 
+    // Обробка події таймера
     @Override
     public void actionPerformed(ActionEvent e) {
         if (inGame) {
@@ -134,10 +176,12 @@ public class GameField extends JPanel implements ActionListener {
         repaint();
     }
 
+    // Встановлення слухача клавіш
     private void setKeyAdapter() {
         addKeyListener(new FieldKeyListener());
     }
 
+    // Клас для обробки клавіш
     private class FieldKeyListener extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
@@ -145,6 +189,7 @@ public class GameField extends JPanel implements ActionListener {
             updateDirection(key);
         }
 
+        // Оновлення напрямку руху в залежності від натисканої клавіші
         private void updateDirection(int key) {
             left = key == KeyEvent.VK_LEFT && !right;
             right = key == KeyEvent.VK_RIGHT && !left;
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,24 +1,28 @@
 import javax.swing.*;
 
+// Клас вікна гри
 public class MainWindow extends JFrame {
 
+    // Конструктор класу
     public MainWindow() {
-        initializeUI();
+        initializeUI(); // Ініціалізація інтерфейсу користувача
     }
 
+    // Метод для налаштування інтерфейсу користувача
     private void initializeUI() {
-        setTitle("Snake Game");
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(320, 345);
-        setLocation(400, 400);
-
-        add(new GameField());
-
-        setVisible(true);
+        setTitle("Змейка"); // Встановлення заголовку вікна
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Встановлення операції при закритті вікна
+        setSize(320, 345); // Встановлення розміру вікна
+        setLocation(400, 400); // Встановлення розташування вікна
+        add(new GameField()); // Додавання ігрового поля до вікна
+        setVisible(true); // Робить вікно видимим
     }
 
+    // Точка входу в програму
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(MainWindow::new);
+        // Запуск GUI в потоці обробки подій
+        SwingUtilities.invokeLater(() -> new MainWindow());
     }
 }
+
 
